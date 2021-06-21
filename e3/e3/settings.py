21eadbe06abdcf3ce71d3787b584b308999e9519
Cristian Silva <cris.silval@duocuc.cl>
2021-06-21 13:29:42 -0400

    bd

--- e3/e3/settings.py
+++ e3/e3/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'e3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'localhost:1521',
+        'USER': 'c##bapp',
+        'PASSWORD': 'bapp', 
+         'TEST': {
+            'USER': 'default_test',
+            'TBLSPACE': 'default_test_tbls',
+            'TBLSPACE_TMP': 'default_test_tbls_tmp',
+        },
+
+    },
 }
 
 
