cea1e56bf9bd9f9b0f3c95f4a4833328aa243535
Cristian Silva <cris.silval@duocuc.cl>
2021-06-21 13:32:19 -0400

    model

--- e3/core/models.py
+++ e3/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    idCategoria = models.IntegerField(primary_key=True, verbose_name= 'Id  Categoria')
+    nombreCategoria =models.CharField(max_length=50, verbose_name= 'Nombre')
+    def __str__(self):
+        return self.nombreCategoria
+#modelo para Libros
+class Libro(models.Model):
+    isbn = models.IntegerField(primary_key=True, verbose_name='Id Libro')
+    nombre = models.CharField(max_length =40, verbose_name ='Nombre')
+    autor = models.CharField(max_length =40, verbose_name ='Autor')
+    descripcion =models.CharField(max_length =150, verbose_name ='Descripción')
+    categoria = models.ForeignKey(Categoria, on_delete= models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
