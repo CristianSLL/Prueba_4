5cf9a0c890a715f77f0d5a566158ba7e77fcf3e0
Cristian Silva <cris.silval@duocuc.cl>
2021-07-03 19:59:53 -0400

    creando metodo put delete

--- e3/rest_libro/urls.py
+++ e3/rest_libro/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from rest_libro.views import lista_libros
+from rest_libro.views import lista_libros, detalle_libro
 
 urlpatterns = [
     path('lista_libros', lista_libros, name="lista_libros"),
+    path('detalle_libro', detalle_libro, name="detalle_libro")
 
 ]
