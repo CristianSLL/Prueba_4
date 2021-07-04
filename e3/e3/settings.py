4335319ba0064c36295eedbb74b71ccb557cf7b6
Cristian Silva <cris.silval@duocuc.cl>
2021-07-03 21:26:52 -0400

    CANDADO Y AUNTENTICado

--- e3/e3/settings.py
+++ e3/e3/settings.py
@@ -136,3 +136,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAUL_AUTHENTICATION_CLASSES':[
+        'rest_framework.authentication.TokenAuthentication',
+
+    ],
+}
--- e3/rest_libro/views.py
+++ e3/rest_libro/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 from core.models import Libro
 from .serializers import LibroSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 @csrf_exempt
 @api_view(['GET', 'POST'])
 def lista_libros(request):
